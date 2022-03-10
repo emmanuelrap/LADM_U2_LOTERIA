79712a606a3eb3b8b06bf6e88ad6f7bb99c37622
Zambrano <emmanuel@gmail.com>
2022-03-10 08:24:47 -0700

    imprime bien arreglo

--- app/src/main/java/com/example/ladm_u2_loteria/HiloBasico.kt
+++ app/src/main/java/com/example/ladm_u2_loteria/HiloBasico.kt
@@ -1,23 +1,37 @@
 package com.example.ladm_u2_loteria
 
 import android.widget.TextView
+import kotlin.random.Random
 
-class HiloBasico (et: TextView) : Thread() {
-        private var pausar =false
-        private var ejecutar = true
-        var index =0
+class HiloBasico (act:MainActivity) : Thread() {
+    var act=act
+    var cartasQuedan=10;
+    val numbers = mutableListOf("carta1","carta2","carta3","carta4","carta5","carta6","carta7","carta8","carta9","carta10")
+
+    private var pausar =false
+    private var ejecutar = true
 
         override fun run() {
             super.run()
+             while(ejecutar){
 
-            while(ejecutar){
-                if(!pausar){
-                    //se pone lo que quiero que ejecute
-
-                }
-                sleep(500)
+                do {
+                    if(cartasQuedan>0){
+                        act.runOnUiThread {
+                            var indexRemover = Random.nextInt(cartasQuedan-1)
+                            act.binding.tv.text = act.binding.tv.text.toString() + "\n" + numbers[indexRemover]
+                            act.binding.tv2.text =act.binding.tv2.text.toString() + "\n" + indexRemover.toString()
+                            numbers.removeAt(indexRemover)
+                           }
+                            cartasQuedan--
+                            sleep(1000)
+                    }
+                } while (cartasQuedan>0)
+                 sleep(100000)
+                pausarHilo()
             }
         }
+
         fun terminarHilo(){
             ejecutar=false
         }
--- app/src/main/java/com/example/ladm_u2_loteria/MainActivity.kt
+++ app/src/main/java/com/example/ladm_u2_loteria/MainActivity.kt
@@ -7,35 +7,17 @@ import com.example.ladm_u2_loteria.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    var cartasQuedan=10;
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding: ActivityMainBinding
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        var tv = findViewById<TextView>(R.id.tv)
+       var hilo1=HiloBasico(this)
+        hilo1.start()
 
-        val numbers = mutableListOf("1","2","3","4","5","6","7","8","9","0")
-
-        do {
-            var indexRemover = Random.nextInt(cartasQuedan)
-            /*
-            if (numbers.removeAt(indexRemover)) {
-
-                tv.text=tv.text.toString()+"\n$indexRemover eliminado"
-            } else {
-                tv.text=tv.text.toString()+"\n Coleccion Vacia"
-            }*/
-
-            if(indexRemover<numbers.size){
-                tv.text=tv.text.toString()+"\n"+numbers[indexRemover]
-                numbers.removeAt(indexRemover)
-                cartasQuedan--
-            }
-        } while (numbers.size>0)
 
     }
 }
