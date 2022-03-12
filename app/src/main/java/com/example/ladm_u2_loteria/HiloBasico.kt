f6ab39e6e058c00d44a26aa26718c7e7eaa6ea09
Zambrano <emmanuel@gmail.com>
2022-03-11 23:41:13 -0700

    Funcional todo

--- app/src/main/java/com/example/ladm_u2_loteria/HiloBasico.kt
+++ app/src/main/java/com/example/ladm_u2_loteria/HiloBasico.kt
@@ -1,33 +1,60 @@
 package com.example.ladm_u2_loteria
 
+import android.media.MediaPlayer
+import android.util.Log
 import android.widget.TextView
 import kotlin.random.Random
 
 class HiloBasico (act:MainActivity) : Thread() {
+    var cartasQuedan=6
     var act=act
-    var cartasQuedan=10;
-    val numbers = mutableListOf("carta1","carta2","carta3","carta4","carta5","carta6","carta7","carta8","carta9","carta10")
-
     private var pausar =false
     private var ejecutar = true
+    val arregloCartas = mutableListOf("El Diablito","La Dama","El catrin","El Paraguas","La Escalera","La Botella")
+    var arregloImagenes = mutableListOf(R.drawable.carta2,R.drawable.carta3,R.drawable.carta4,R.drawable.carta5,R.drawable.carta7,R.drawable.carta8)
+    var arregloSonidos = mutableListOf(R.raw.carta2,R.raw.carta3,R.raw.carta4,R.raw.carta5,R.raw.carta7,R.raw.carta8)
+
 
         override fun run() {
+
             super.run()
              while(ejecutar){
 
                 do {
-                    if(numbers.size>0){
-                        act.runOnUiThread {
-                            var indexRemover = Random.nextInt(numbers.size)
-                            act.binding.tv.text = act.binding.tv.text.toString() + "\n" + numbers[indexRemover]
-                            act.binding.tv2.text =act.binding.tv2.text.toString() + "\n" + indexRemover.toString()
-                            act.binding.tv3.text =act.binding.tv3.text.toString() + "\n" + numbers.size.toString()
-                            numbers.removeAt(indexRemover)
-                        }
-                            sleep(1000)
+                    while(pausar){
+                        sleep(500)
                     }
-                } while (numbers.size>0)
-                pausarHilo()
+                    if(arregloCartas.size>0 ){
+                        act.runOnUiThread {
+                            //Random
+                            var indexRemover = Random.nextInt(arregloCartas.size)
+                            //Sonido
+                            act.mp= MediaPlayer.create(act,arregloSonidos[indexRemover])
+                            act.mp?.start()
+                            //Imagenes
+                            act.binding.imagen.setImageResource(arregloImagenes[indexRemover])
+                            act.binding.tv.text =  arregloCartas[indexRemover]
+                            //Removicion de los que salieron
+                            arregloCartas.removeAt(indexRemover)
+                            arregloImagenes.removeAt(indexRemover)
+                            arregloSonidos.removeAt(indexRemover)
+                            cartasQuedan--
+                        }
+                            sleep(3000)
+                    }
+                } while (arregloCartas.size>0)
+               // pausarHilo()
+            }
+        }
+
+        fun mostrar(){
+            act.runOnUiThread {
+                var aux=cartasQuedan
+                while (aux>0){
+                    act.binding.tvFaltantes.text = act.binding.tvFaltantes.text.toString()+ "\n"+arregloCartas[aux-1]
+                    aux--
+
+                }
             }
         }
 
--- app/src/main/java/com/example/ladm_u2_loteria/MainActivity.kt
+++ app/src/main/java/com/example/ladm_u2_loteria/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.widget.TextView
 import com.example.ladm_u2_loteria.databinding.ActivityMainBinding
 import kotlin.random.Random
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
+    var mp:MediaPlayer?=null
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +18,23 @@ class MainActivity : AppCompatActivity() {
 
 
        var hilo1=HiloBasico(this)
-        hilo1.start()
 
+        binding.btnIniciar.setOnClickListener {
+            hilo1.start()
+            binding.btnIniciar.isEnabled=false
+        }
+
+        binding.btnDetener.setOnClickListener {
+            if(hilo1.estaPausado())
+                hilo1.despausarHilo()
+            else
+                hilo1.pausarHilo()
+
+        }
+
+        binding.btnMostrar.setOnClickListener {
+            hilo1.mostrar()
+        }
 
     }
 }
